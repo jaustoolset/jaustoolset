dcbea63696f69a7536ed6f875e61bac2bdea8631
DaveMartin <davem@neyasystems.com>
2015-09-17 11:31:23 -0500

    Bug fix for Linux since timer ID is allowed to be zero

--- GUI/templates/Common/src/Transport/OS.cpp
+++ GUI/templates/Common/src/Transport/OS.cpp
@@ -44,7 +44,7 @@ void DeVivo::Junior::JrSpawnProcess(std::string path, std::string arg)
 #ifdef WINDOWS
     // Windows gives us the createProcess function.  We just need to
     // make sure it doesn't already exist.
-	std::string cmd = "tasklist | findstr " + path + " > nul\0";
+    std::string cmd = "tasklist | findstr " + path + " > nul\0";
     int ret = system(cmd.c_str());
 
     if (ret != 0)
@@ -60,35 +60,35 @@ void DeVivo::Junior::JrSpawnProcess(std::string path, std::string arg)
         si.wShowWindow = SW_HIDE;
 
         // Now create the process that points to the path
-		cmd = path + " " + arg + "\0";
+        cmd = path + " " + arg + "\0";
         BOOL result = CreateProcess(  NULL, LPSTR(cmd.c_str()), NULL, NULL, FALSE, 
             HIGH_PRIORITY_CLASS | CREATE_NEW_PROCESS_GROUP, NULL,  NULL,  &si, &pi);
         if(result == 0)  
             JrError << "Could not create process " << path << std::endl;
 
-		// Wait for it to spool up before returning
-		JrSleep(2000);
+        // Wait for it to spool up before returning
+        JrSleep(2000);
     }
 
 #else
 
     // For Unix, we need to fork and manually start the new process
     // First check for existence of the process
-	std::string cmd = "ps -e | grep '" + path + "' | grep -v 'grep' >> /dev/null\0";
+    std::string cmd = "ps -e | grep '" + path + "' | grep -v 'grep' >> /dev/null\0";
     int ret = system(cmd.c_str());
     if (ret != 0)
     {
         // Not found.  Start a new process...
-		JrInfo << "Starting " << path << std::endl;
+        JrInfo << "Starting " << path << std::endl;
         if (fork()==0)
-		{
+        {
            ret = execl(path.c_str(), path.c_str(), arg.c_str(), NULL);
-		   if (ret == -1) exit(0); // Getting here means we failed to start the process
-		}
+           if (ret == -1) exit(0); // Getting here means we failed to start the process
+        }
 
 
-		// Wait for it to spool up before returning
-		JrSleep(2000);
+        // Wait for it to spool up before returning
+        JrSleep(2000);
     }
 
 #endif
@@ -123,7 +123,7 @@ std::list<unsigned int> DeVivo::Junior::JrGetIPAddresses()
 		{
             for (int i = 0; phe->h_addr_list[i] != 0; ++i) 
                 addresses.push_back(((in_addr*)phe->h_addr_list[i])->s_addr);
-		}
+    }
 		else
 		{
 			JrError
@@ -183,14 +183,14 @@ int DeVivo::Junior::JrSpawnThread(void*(*func_ptr)(void*), void* func_arg)
     return (int) CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) func_ptr, func_arg, 0, NULL);
 #else
     pthread_t thread_info;
-	pthread_attr_t attr;
-	pthread_attr_init(&attr);
-	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);	
-	if(pthread_create(&thread_info, &attr, func_ptr, func_arg) != 0)
-		JrError << "Could not create thread\n";
-	pthread_attr_destroy(&attr);
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);    
+    if(pthread_create(&thread_info, &attr, func_ptr, func_arg) != 0)
+        JrError << "Could not create thread\n";
+    pthread_attr_destroy(&attr);
 #ifdef __MAC__
-	return 0;
+    return 0;
 #else
     return (int) thread_info;
 #endif
@@ -200,8 +200,8 @@ int DeVivo::Junior::JrSpawnThread(void*(*func_ptr)(void*), void* func_arg)
 void DeVivo::Junior::JrKillThread(int thread)
 {
 #ifdef WINDOWS
-	TerminateThread((HANDLE) thread, 0);
-	CloseHandle((HANDLE) thread);
+    TerminateThread((HANDLE) thread, 0);
+    CloseHandle((HANDLE) thread);
 #else
     pthread_join((pthread_t) thread, NULL);
 #endif
@@ -210,18 +210,18 @@ void DeVivo::Junior::JrKillThread(int thread)
 DeVivo::Junior::JrMutex::JrMutex()
 {
 #ifdef WINDOWS
-	mutex = CreateMutex(NULL, false, NULL);
+    mutex = CreateMutex(NULL, false, NULL);
 #else
-	pthread_mutex_init(&mutex, NULL);
+    pthread_mutex_init(&mutex, NULL);
 #endif
 }
 
 DeVivo::Junior::JrMutex::~JrMutex()
 {
 #ifdef WINDOWS
-	if (mutex) CloseHandle(mutex);
+    if (mutex) CloseHandle(mutex);
 #else
-	pthread_mutex_destroy(&mutex);
+    pthread_mutex_destroy(&mutex);
 #endif
 }
 
@@ -230,58 +230,58 @@ void DeVivo::Junior::JrMutex::lock()
 #ifdef WINDOWS
     WaitForSingleObject(mutex, INFINITE);
 #else
-	pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&mutex);
 #endif
 }
 
 void DeVivo::Junior::JrMutex::unlock()
 {
 #ifdef WINDOWS
-	ReleaseMutex(mutex);
+    ReleaseMutex(mutex);
 #else
-	pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&mutex);
 #endif
 }
 
 DeVivo::Junior::JrSignal::JrSignal()
 {
 #ifdef WINDOWS
-	condvar = CreateSemaphore(NULL, 0, 0x7fffffff, NULL);
+    condvar = CreateSemaphore(NULL, 0, 0x7fffffff, NULL);
 #else
-	pthread_mutex_init(&mutex, NULL);
-	pthread_cond_init(&condvar, NULL);
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&condvar, NULL);
 #endif
 }
 
 DeVivo::Junior::JrSignal::~JrSignal()
 {
 #ifdef WINDOWS
-	if (condvar) CloseHandle(condvar);
+    if (condvar) CloseHandle(condvar);
 #else
-	pthread_mutex_destroy(&mutex);
-	pthread_cond_destroy(&condvar);
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&condvar);
 #endif
 }
 
 void DeVivo::Junior::JrSignal::wait()
 {
 #ifdef WINDOWS
-	WaitForSingleObject(condvar, INFINITE);
+    WaitForSingleObject(condvar, INFINITE);
 #else
-	pthread_mutex_lock(&mutex);
-	pthread_cond_wait(&condvar, &mutex);
-	pthread_mutex_unlock(&mutex);
+    pthread_mutex_lock(&mutex);
+    pthread_cond_wait(&condvar, &mutex);
+    pthread_mutex_unlock(&mutex);
 #endif
 }
 
 void DeVivo::Junior::JrSignal::signal()
 {
 #ifdef WINDOWS
-	ReleaseSemaphore(condvar, 1, NULL);
+    ReleaseSemaphore(condvar, 1, NULL);
 #else
-	pthread_mutex_lock(&mutex);
-	pthread_cond_signal(&condvar);
-	pthread_mutex_unlock(&mutex);
+    pthread_mutex_lock(&mutex);
+    pthread_cond_signal(&condvar);
+    pthread_mutex_unlock(&mutex);
 #endif
 }
 
@@ -293,112 +293,110 @@ VOID CALLBACK TimerRoutine(PVOID arg, BOOLEAN TimerOrWaitFired)
 #elif defined(__MAC__)
 static void* watchForTimeout(void* arg)
 {
-	// Brute force.  Sleep until we pass the elapsed time.
-	unsigned long start = JrGetTimestamp();
-	while ((JrGetTimestamp() - start) < ((DeVivo::Junior::JrTimer*) arg)->getTimeout())
-		DeVivo::Junior::JrSleep(1);
+    // Brute force.  Sleep until we pass the elapsed time.
+    unsigned long start = JrGetTimestamp();
+    while ((JrGetTimestamp() - start) < ((DeVivo::Junior::JrTimer*) arg)->getTimeout())
+        DeVivo::Junior::JrSleep(1);
     ((DeVivo::Junior::JrTimer*) arg)->call_user_function();
-	return 0;
+    return 0;
 }
 #else
 static void handleTimeout(sigval_t sig)
 {
-	// We don't want the user to have to mess with the
-	// stupid signal_t stuff, so provide translation.
-	((DeVivo::Junior::JrTimer*) sig.sival_ptr)->call_user_function();
+    // We don't want the user to have to mess with the
+    // stupid signal_t stuff, so provide translation.
+    ((DeVivo::Junior::JrTimer*) sig.sival_ptr)->call_user_function();
 }
 #endif
 
 
 DeVivo::Junior::JrTimer::JrTimer(void (*func_ptr)(void*), 
-								 void* func_arg, 
-								 unsigned int timeout)
+                                 void* func_arg, 
+                                 unsigned int timeout)
 {
-	this->func_ptr = func_ptr;
-	this->func_arg = func_arg;
-	timeout_ms = timeout;
+    this->func_ptr = func_ptr;
+    this->func_arg = func_arg;
+    timeout_ms = timeout;
 
 #if defined(WINDOWS) 
-	tHandle = 0;
+    tHandle = 0;
 #elif defined(__MAC__)
-	threadId = 0;
+    threadId = 0;
 #else
-	timerid = 0;
-	struct sigevent sev;
-	sev.sigev_notify = SIGEV_THREAD;
-	sev.sigev_notify_function = handleTimeout;
+    timerid = 0;
+    struct sigevent sev;
+    sev.sigev_notify = SIGEV_THREAD;
+    sev.sigev_notify_function = handleTimeout;
     sev.sigev_value.sival_ptr = this;
-	sev.sigev_notify_attributes = NULL;
-    timer_create(CLOCK_REALTIME, &sev, &timerid);
+    sev.sigev_notify_attributes = NULL;
+    int result = timer_create(CLOCK_REALTIME, &sev, &timerid);
 #endif
 }
 
 DeVivo::Junior::JrTimer::~JrTimer()
 {
 #if defined(WINDOWS) || defined(__MAC__)
-	stop();
+    stop();
 #else
-	if (timerid != 0) timer_delete(timerid);
+    timer_delete(timerid);
 #endif
 }
 
 void DeVivo::Junior::JrTimer::start()
 {
-	// stop any existing timers...
-	stop();
+    // stop any existing timers...
+    stop();
 
 #if defined(WINDOWS)
-	CreateTimerQueueTimer( &tHandle, NULL, TimerRoutine, this, timeout_ms, 0, 0);
+    CreateTimerQueueTimer( &tHandle, NULL, TimerRoutine, this, timeout_ms, 0, 0);
 #elif defined(__MAC__)
-	// Spawn a thread that sleeps for the requested duration.
-	// We do this since the Mac API doesn't have a lot
-	// of good options for asynchronous timers.
-	threadId = DeVivo::Junior::JrSpawnThread(watchForTimeout, this);
+    // Spawn a thread that sleeps for the requested duration.
+    // We do this since the Mac API doesn't have a lot
+    // of good options for asynchronous timers.
+    threadId = DeVivo::Junior::JrSpawnThread(watchForTimeout, this);
 #else
-	// posix timers.
-	struct itimerspec interval;
-	interval.it_value.tv_sec = timeout_ms / 1000;
-	interval.it_value.tv_nsec = (timeout_ms % 1000) * 1000000;
-	interval.it_interval.tv_sec = 0;
-	interval.it_interval.tv_nsec = 0;
+    // posix timers.
+    struct itimerspec interval;
+    interval.it_value.tv_sec = timeout_ms / 1000;
+    interval.it_value.tv_nsec = (timeout_ms % 1000) * 1000000;
+    interval.it_interval.tv_sec = 0;
+    interval.it_interval.tv_nsec = 0;
 
-	if (timerid != 0)
-		timer_settime(timerid, 0, &interval, NULL);
+    timer_settime(timerid, 0, &interval, NULL);
 #endif
 }
 
 void DeVivo::Junior::JrTimer::stop()
 {
 #if defined(WINDOWS) 
-	if (tHandle != 0) 
-	{
-		DeleteTimerQueueTimer(NULL, tHandle, NULL);
-		CloseHandle(tHandle);
-		tHandle = 0;
-	}
+    if (tHandle != 0) 
+    {
+        DeleteTimerQueueTimer(NULL, tHandle, NULL);
+        CloseHandle(tHandle);
+        tHandle = 0;
+    }
 #elif defined(__MAC__)
     if (threadId != 0) 
-	{
-		JrKillThread(threadId);
-		threadId = 0;
-	}
+    {
+        JrKillThread(threadId);
+        threadId = 0;
+    }
 #else
-	struct itimerspec interval;
-	interval.it_value.tv_sec = 0;
-	interval.it_value.tv_nsec = 0;
-	interval.it_interval.tv_sec = 0;
-	interval.it_interval.tv_nsec = 0;
+    struct itimerspec interval;
+    interval.it_value.tv_sec = 0;
+    interval.it_value.tv_nsec = 0;
+    interval.it_interval.tv_sec = 0;
+    interval.it_interval.tv_nsec = 0;
 
-	if (timerid != 0)
-		timer_settime(timerid, 0, &interval, NULL);
+    timer_settime(timerid, 0, &interval, NULL);
 #endif
 }
 
 void DeVivo::Junior::JrTimer::call_user_function()
 {
 #if defined(__MAC__)
-	// thread died on run to completion.
-	threadId = 0;
+    // thread died on run to completion.
+    threadId = 0;
 #endif
-	func_ptr(func_arg);
+    func_ptr(func_arg);
 }
